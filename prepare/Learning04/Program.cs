2ecb9272139562aa0ae7973d811d38f9ba56094d
maylinabull <maylinabull13@gmail.com>
2023-06-01 23:56:35 -0600

    INHERITANCE

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Maylina Bull", "Division");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Maylina Bull", "Division","7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
+
     }
 }
